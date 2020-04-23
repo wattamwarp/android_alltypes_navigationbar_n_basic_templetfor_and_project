151ebd9228cf6ec6f8a97b25d3afa1a71bdd6a42
padmanabh <wattamwarp874@gmail.com>
2020-04-23 18:26:08 +0530

    added side navigation drawer in app

--- app/src/main/java/com/gnvc/MainActivity.java
+++ app/src/main/java/com/gnvc/MainActivity.java
@@ -2,16 +2,28 @@ package com.gnvc;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.ToolbarWidgetWrapper;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 //import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerlayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toogle;
+    NavigationView  navigation_view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,31 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         //getting bottom navigation view and attaching the listener
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
         navigation.setOnNavigationItemSelectedListener(MainActivity.this);
+
+        settoolbar();//creating toolbar icon
+
+        navigation_view=findViewById(R.id.navigation_menu);
+        navigation_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment1 = null;
+                switch (item.getItemId()){
+
+                    case R.id.drawaer_nav_home:
+                    fragment1=new fragment_home();
+                    break;
+
+                    case  R.id.drawaer_nav_about:
+                        fragment1=new fragment_account();
+                        break;
+
+                }
+
+                return loadFragment(fragment1);
+            }
+        });
+
+
     }
 
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -62,4 +99,17 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
+
+    public void settoolbar(){
+        drawerlayout=findViewById(R.id.drawer_layout);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toogle=new ActionBarDrawerToggle(this,drawerlayout,toolbar,R.string.app_name,R.string.app_name);
+        drawerlayout.setDrawerListener(toogle);
+        toogle.syncState();
+
+
+    }
+
 }
